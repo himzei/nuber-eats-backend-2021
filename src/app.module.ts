62f5729f312fbc42828259632392e25fb747d18a
himzei <himzei@gmail.com>
2021-04-22 16:49:45 +0900

    5.1 Generating JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { CommonModule } from './common/common.module';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRoot({
