10d2bdeb71bcbcb478afce65e675b15dc36e25d7
himzei <himzei@gmail.com>
2021-04-23 19:03:31 +0900

    7.2 Mocking part Two

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,20 +1,66 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { getRepository, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      poviders: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount ');
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {});
+  });
   it.todo('login ');
   it.todo('findById ');
   it.todo('editProfile ');
