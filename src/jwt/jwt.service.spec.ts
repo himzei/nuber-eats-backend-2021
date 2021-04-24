bb375d2941e80a38849b2a4ea22f70247952c358
himzei <himzei@gmail.com>
2021-04-24 14:15:47 +0900

    8.1 JWT Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -12,15 +19,28 @@ describe('JwtService', () => {
         JwtService,
         {
           provide: CONFIG_OPTIONS,
-          useValue: { privatekey: TEST_KEY },
+          useValue: { privateKey: TEST_KEY },
         },
       ],
     }).compile();
     service = module.get<JwtService>(JwtService);
   });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it.todo('sing');
-  it.todo('verify');
+
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenLastCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+
+  describe('verify', () => {
+    it('should return the decoded token', () => {});
+  });
 });
