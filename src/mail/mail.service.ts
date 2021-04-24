2ea1d7a49481c8b508e2e3987bbecc24301df23b
himzei <himzei@gmail.com>
2021-04-24 14:58:24 +0900

    8.3 sendVerificationEmail Test

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -8,22 +8,9 @@ import { EmailVar, MailModuleOptions } from './mail.interfaces';
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    // this.sendEmail('test', 'content')
-    //   .then(() => {
-    //     console.log('sent mail');
-    //   })
-    //   .catch((error) => {
-    //     console.log(error.response.body);
-    //   });
-  }
+  ) {}
 
-  private async sendEmail(
-    subject: string,
-
-    template: string,
-    emailVars: EmailVar[],
-  ) {
+  async sendEmail(subject: string, template: string, emailVars: EmailVar[]) {
     const form = new FormData();
     form.append(
       'from',
