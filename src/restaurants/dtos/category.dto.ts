898fd4486e65605d9693a23a4ec8b98998927346
himzei <himzei@gmail.com>
2021-04-29 19:43:51 +0900

    10.16 Restaurants

--- src/restaurants/dtos/category.dto.ts
+++ src/restaurants/dtos/category.dto.ts
@@ -1,10 +1,10 @@
-import { ArgsType, Field, InputType, ObjectType } from '@nestjs/graphql';
-import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import {
   PaginationInput,
   PaginationOutput,
 } from 'src/common/dtos/pagination.dto';
 import { Category } from '../entities/category.entity';
+import { Restaurant } from '../entities/restaurant.entity';
 
 @InputType()
 export class CategoryInput extends PaginationInput {
@@ -14,6 +14,9 @@ export class CategoryInput extends PaginationInput {
 
 @ObjectType()
 export class CategoryOutput extends PaginationOutput {
+  @Field((type) => [Restaurant], { nullable: true })
+  restaurants?: Restaurant[];
+
   @Field((type) => Category, { nullable: true })
   category?: Category;
 }
